2c4d49f4588890d15c81bc001a97ab3290160b94
lnagy2002 <kavoi2011@gmail.com>
2025-10-08 11:02:32 -0700

    Update generate-images-json.js

--- scripts/generate-images-json.js
+++ scripts/generate-images-json.js
@@ -22,7 +22,7 @@ function yyyymmdd(d = new Date()) {
   const yyyy = d.getFullYear();
   const mm = String(d.getMonth() + 1).padStart(2, "0");
   const dd = String(d.getDate()).padStart(2, "0");
-  return `${yyyy}${mm}${dd}`;
+  return `${mm}${dd}${yyyy}`;
 }
 function slug(s) {
   return s.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");

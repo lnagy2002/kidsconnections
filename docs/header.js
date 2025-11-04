f4d047f7ac12a722f0b5c455735747ca79ab9cc8
lnagy2002 <kavoi2011@gmail.com>
2025-11-04 11:51:15 -0800

    some header  changes

--- docs/header.js
+++ docs/header.js
@@ -4,22 +4,21 @@
   const container = script.parentElement;
   container.innerHTML = `
     <section id="header-container">
-      <img src="https://bittobitscom.wordpress.com/wp-content/uploads/2025/09/logo-yellow.png" alt="" height="60px">
+      <!-- <img src="https://bittobitscom.wordpress.com/wp-content/uploads/2025/09/logo-yellow.png" alt="" height="60px"> -->
       <div>
         <h2>${title}</h2>
         <div id="date"></div>
       </div>
     </section>
-    <hr>
   `;
 
   // === Date + Level Logic ===
   const t = new Date();
-  
+
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   const formattedDate = t.toLocaleDateString(undefined, options);
 
-  const mm = String(t.getMonth() + 1).padStart(2, '0'); 
+  const mm = String(t.getMonth() + 1).padStart(2, '0');
   const dd = String(t.getDate()).padStart(2, '0');
   const yyyy = t.getFullYear();
 
@@ -27,4 +26,3 @@
   const level = getQueryParam('level') || 'easy';
 
   document.getElementById("date").textContent = `Daily ${level} • ${formattedDate}`;
-

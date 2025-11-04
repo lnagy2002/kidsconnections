f38aa84286250355cb2ebf995aca3019bc3c1ea2
lnagy2002 <kavoi2011@gmail.com>
2025-11-04 11:18:59 -0800

    added  footer

---
+++ docs/footer.js
@@ -0,0 +1,13 @@
+  const script = document.currentScript;
+
+  const container = script.parentElement;
+  container.innerHTML = `
+  <footer class="footer">
+  <img
+    src="https://bittobitscom.wordpress.com/wp-content/uploads/2025/09/logo-yellow.png"
+    alt="The Mind Bits"
+    class="footer-logo"
+  />
+  <div class="footer-text">©2025 The Mind Bits</div>
+</footer>
+  `;
